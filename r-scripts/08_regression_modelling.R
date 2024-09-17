e7f4c79e9f1a52ec5993bada49810a1b9871334e
Karsten Schoonbeek <k.schoonbeek@student.rug.nl>
2024-09-17 10:36:02 +0200

    tuesday

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -191,12 +191,28 @@ p1+geom_line(data=orchdat3,aes(y=pred6),linewidth=1.2)
 # add the interaction to the model: elevation + elevation ^2 + year + elevation*year
 # now test and show  the effect of both elevation + year
 
+m7<-glm(CountSum~elevation_m+I(elevation_m^2)+factor(year),
+        family=poisson(log),
+        data=orchdat3)
+orchdat3$pred7<-predict(m7, type = "response")
+p1+geom_line(data=orchdat3,aes(y=pred7),linewidth=1.2)
 
 #add the  model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
 
+m7<-glm(CountSum~elevation_m+I(elevation_m^2)+factor(year),
+        family=poisson(log),
+        data=orchdat3)
+orchdat3$pred7<-predict(m7, type = "response")
+p1+geom_line(data=orchdat3,aes(y=pred7),linewidth=1.2)
 
+anova(m7,m6,test="Chisq")
 
+m8<-glm(CountSum~elevation_m+I(elevation_m^2)+factor(year)+elevation_m*factor(year),
+        family=poisson(log),
+        data=orchdat3)
+orchdat3$pred8<-predict(m8, type = "response")
+p1+geom_line(data=orchdat3,aes(y=pred8),linewidth=1.2)
 
-
+anova(m8,m7,test="Chisq")
